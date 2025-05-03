f93b48eae686589006939e0a660fb65c67a4f583
Mukul Anand <mukulanand@Mukuls-MacBook-Air.local>
2025-05-04 00:19:14 +0530

    Basic Authentication

---
+++ src/main/java/com/mas/spring/seurity/springsecurityapp/config/MySecurityConfig.java
@@ -0,0 +1,38 @@
+package com.mas.spring.seurity.springsecurityapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MySecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("mukul")
+                .password(passwordEncoder().encode("anand"))
+                .authorities("read").build();
+        userDetailsService.createUser(userDetails);
+        return userDetailsService;
+    }
+}
---
+++ src/test/java/com/mas/spring/seurity/springsecurityapp/SpringsecurityappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.spring.seurity.springsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
